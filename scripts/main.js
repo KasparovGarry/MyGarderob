7b228e7cef0103fffcafaf43f73c1290d607ea49
Garry Kasparov <cgompany519yt@gmail.com>
2020-08-16 02:54:52 +0400

    deploy

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,28 @@
-$(".reviews-fixed").click(function(){
+const navBar = $("#navbarSupportedContent");
+const reviews = $(".reviews-fixed");
+
+reviews.click(function () {
     $(".reviews-fixed").toggleClass("show");
 });
+
+document.querySelectorAll(".nav-item")
+    .forEach((navItem) => {
+        navItem.addEventListener("click", () => {
+            if (navBar.hasClass("show")) {
+                navBar.removeClass("show")
+            }
+        })
+    })
+
+document.querySelectorAll(".responsiveClose")
+    .forEach((responsiveClose) => {
+        responsiveClose.addEventListener("click", () => {
+            if (navBar.hasClass("show")) {
+                reviews.removeClass("show")
+            }
+
+            if (reviews.hasClass("show")) {
+                navBar.removeClass("show")
+            }
+        })
+    })
